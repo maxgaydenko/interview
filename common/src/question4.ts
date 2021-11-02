91475402ff30cbd60600b767b44b87f7dbe06f7e
Max Gaydenko <max@gaydenko.ru>
2021-11-02 19:10:18 +0300

    Interview

--- common/src/question4.ts
+++ common/src/question4.ts
@@ -2,7 +2,7 @@ interface IResult {
  resultValue: number
 }
 
-function someFunction(value: number) {
+function someFunction(value: number): Promise<IResult> {
  return new Promise<IResult>((resolve) =>
    setTimeout(() => resolve({ resultValue: value + 1 }), 500)
  )
